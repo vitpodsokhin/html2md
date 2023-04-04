4ab7cb8965e2c44fc2c7a33e30defdc98231b7b5
vit <vitpodsokhin@gmail.com>
2023-04-05 00:18:35 +0300

    This version works

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, request
 import html2markdown
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST' and 'html_string' in request.form:
-        html_string = request.form['html_string']
+    if request.method == 'POST' and 'htmlString' in request.form:
+        html_string = request.form['htmlString']
         markdown_string = html2markdown.convert(html_string)
-        return render_template('index.html', markdown_string=markdown_string)
+        return markdown_string
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
